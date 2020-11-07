bebe30bf31272de1d5c179ab66a4e1a8272fd52a
Martyn Kaminetskyi <Martyn.Kaminetskyi.kn.2017@lpnu.ua>
2020-11-07 14:29:37 +0200

    Added main container and header. Added basic styles for them.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import './Header.scss'
+import { Link, Route, Switch } from 'react-router-dom';
+import UserPageContainer from '../UserPage/UserPageContainer';
+import TicketPageContainer from '../TicketPage/TicketPageContainer';
+import RecycleBin from '../RecycleBin/RecycleBinContainer'
+
 
 const Header = (props) => {
     return(
         <div>
-
+        <div className="container">
+            <nav className="navbar-light">
+                <ul className="navbar-nav pages">
+                    <li>
+                        <Link className="nav-item" to='../UserPage/UserPageContainer'>Users</Link>
+                    </li>
+                    <li>
+                        <Link className="nav-item" to='../TicketPage/TicketPageContainer'>Tickets</Link>
+                    </li>
+                </ul>
+                <ul className="navbar-nav recycle-bin">
+                    <li>
+                        <Link className="nav-item" to='../RecycleBin/RecycleBinContaine'>Recycle Bin</Link>
+                    </li>
+                </ul>
+            </nav>
+          </div>
+          <Switch>
+            <Route exact path='../UserPage/UserPageContainer'><UserPageContainer /></Route>
+            <Route path='../TicketPage/TicketPageContainer'><TicketPageContainer /></Route>
+            <Route path='../RecycleBin/RecycleBinContaine'><RecycleBin /></Route>
+          </Switch>
         </div>
     )
 }
--- src/Components/RecycleBin/RecycleBinContainer.jsx
+++ src/Components/RecycleBin/RecycleBinContainer.jsx
@@ -4,7 +4,7 @@ class RecycleBinContainer extends React.Component{
     render(){
         return(
             <div>
-
+                <button>Recycle Bin</button>
             </div>
         )
     }
