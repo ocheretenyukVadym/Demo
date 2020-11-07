a5032ca8771e7b06169af50262a8ba4764e82342
ocheretenyukVadym <ocheretenjyk@gmail.com>
2020-11-07 15:35:26 +0200

    S-56: create API.js and store

---
+++ src/Components/Store/store.js
@@ -0,0 +1,152 @@
+import { action, makeObservable, observable } from 'mobx';
+import { ticketAPI, userAPI } from '../../API/API';
+
+export class Store{
+    users = [];
+    tickets = [];
+
+    deletedUsers = [];
+    deletedTickets = [];
+
+    isFetching = true;
+
+    newUserValue = '';
+    newTicketValue = '';
+
+    constructor() {
+        makeObservable(this, {
+            users: observable,
+            tickets: observable,
+            deletedUsers: observable,
+            deletedTickets: observable,
+            isFetching: observable,
+            newUserValue: observable,
+            newTicketValue: observable,
+            setUserValue: action,
+            setTicketValue: action,
+            setUsers: action,
+            setTickets: action,
+            setDeletedUsers: action,
+            setDeletedTickets: action,
+            getUsers: action,
+            createNewUser: action,
+            updateUser: action,
+            deleteUser: action,
+            getDeletedUsers: action,
+            getTickets: action,
+            createNewTicket: action,
+            updateTicket: action,
+            deleteTicket: action,
+            getDeletedTickets: action,
+            assignToUser: action,
+        })
+    }
+
+    setUserValue = newValue => this.newUserValue = newValue;
+
+    setTicketValue = newValue => this.newTicketValue = newValue;
+
+    setUsers = users => this.users = users;
+
+    setTickets = tickets => this.tickets = tickets;
+
+    setDeletedUsers = deletedUsers => this.deletedTickets = deletedUsers;
+
+    setDeletedTickets = deletedTickets => this.deletedTickets = deletedTickets;
+
+    getUsers = () => {
+        this.isFetching = false;
+        userAPI.getUsers().then( data => {
+            this.isFetching = true;
+            this.setUsers(data);
+        })
+    }
+
+    createNewUser = (data) => {
+        this.isFetching = false;
+        userAPI.addUser(data).then( data => {
+            this.isFetching = true;
+            console.log(data);         //temp
+        })
+    }
+
+    updateUser = id => {
+        this.isFetching = false;
+        userAPI.updateUser(id).then( data => {
+            this.isFetching = true;
+            console.log(data);      //temp
+            this.getUsers();
+        })
+    }
+
+    deleteUser = id => {
+        this.isFetching = false;
+        userAPI.deleteUser(id).then( data => {
+            this.isFetching = true;
+            console.log(data);      //temp
+            this.getUsers();
+        })
+    }
+
+    getDeletedUsers = () => {           //doesn`t exist yet
+        this.isFetching = false;
+        userAPI.getDeletedUsers().then( data => {
+            this.isFetching = true;
+            this.setDeletedUsers(data);
+        })
+    }
+    
+    getTickets = () => {
+        this.isFetching = false;
+        ticketAPI.getTickets().then( data => {
+            this.isFetching = true;
+            this.setTickets(data);
+        })
+    }
+
+    createNewTicket = (data) => {
+        this.isFetching = false;
+        ticketAPI.addTicket(data).then( data => {
+            this.isFetching = true;
+            console.log(data);         //temp
+        })
+    }
+
+    updateTicket = id => {
+        this.isFetching = false;
+        ticketAPI.updateTicket(id).then( data => {
+            this.isFetching = true;
+            console.log(data);      //temp
+            this.getTickets();
+        })
+    }
+
+    deleteTicket = id => {
+        this.isFetching = false;
+        ticketAPI.deleteTicket(id).then( data => {
+            this.isFetching = true;
+            console.log(data);      //temp
+            this.getTickets();
+        })
+    }
+
+    getDeletedTickets = () => {           //doesn`t exist yet
+        this.isFetching = false;
+        ticketAPI.getDeletedTickets().then( data => {
+            this.isFetching = true;
+            this.setDeletedTickets(data);
+        })
+    }
+
+    assignToUser = (userId, ticketId) => {            //doesn`t exist yet
+        this.isFetching = false;
+        ticketAPI.assignToUser(userId,ticketId).then( data => {
+            this.isFetching = true;
+            this.getTickets();
+            this.getUsers();
+        })
+    }
+}
+
+
+export const store = new Store();
