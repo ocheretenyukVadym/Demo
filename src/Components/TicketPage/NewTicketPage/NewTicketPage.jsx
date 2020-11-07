b644007ffccd7cf4b58cfbd5d09725c2eb8eec5f
shermet5 <nazar_L2013@mail.ru>
2020-11-07 19:58:44 +0200

    S-8 add new tiket

--- src/Components/TicketPage/NewTicketPage/NewTicketPage.jsx
+++ src/Components/TicketPage/NewTicketPage/NewTicketPage.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
 
 const NewTicketPage = (props) => {
-    return(
-        <div>
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
 
-        </div>
-    )
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  })
+
+  const hendleChange = e => {
+    setInput(e.target.value);
+  }
+  const hendleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+
+      // must be post to db;
+      text: input
+
+    });
+    setInput('');
+  }
+  return (
+    <div className='container'>
+        <input
+          type='text'
+          name='text'
+          onChange={hendleChange}
+          placeholder='Title'
+          className='title-input'
+          ref={inputRef}
+        />
+        <button onClick={hendleSubmit} className='add-button'>Add Ticket</button>
+    </div>
+  )
 }
 
 export default NewTicketPage;
