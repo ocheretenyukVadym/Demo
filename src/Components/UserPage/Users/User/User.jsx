5165eb196dd376844a3c6982b31d0b0617afbd70
ocheretenyukVadym <ocheretenjyk@gmail.com>
2020-11-08 19:03:35 +0200

    S-39 to master: add dropdown tickets to user

--- src/Components/UserPage/Users/User/User.jsx
+++ src/Components/UserPage/Users/User/User.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
+import AllTicketsByUser from './TicketsByUser/AllTicketsByUser';
 import './User.css';
 
 const User = ({ user, updateUser, isUserPage, moveToTrash, deleteUser }) => {
     const [isViewChanges, setIsViewChanges] = useState(false);
     const [inputValue, setInputValue] = useState(user.name);
+    const [isShowTickets, setIsShowTickets] = useState(false);
 
     const clickUpdateUser = () => setIsViewChanges(true);
     const handlerChange = e => setInputValue(e.target.value);
     const moveToRecycleBin = () => moveToTrash(user.id);
     const moveToDeletedUsers = () => deleteUser(user.id);
 
+    const isPresentTickets = () => user.tickets.length != 0;
+
+    const showTicketslist = () => {isShowTickets? setIsShowTickets(false) : setIsShowTickets(true)}
     const saveUser = () => {
         let newUser = { ...user };
         newUser.name = inputValue;
@@ -21,8 +26,12 @@ const User = ({ user, updateUser, isUserPage, moveToTrash, deleteUser }) => {
     return (
         <div>
             {!isViewChanges ?
+                <>
                 <div className="user-container">
-                        <p>{user.name}</p>
+                    {isUserPage && isPresentTickets() && <span onClick={showTicketslist}>🎫</span>}
+                        <p className="user-name" onClick={showTicketslist}>
+                            {user.name}
+                        </p>
                         {isUserPage &&
                             <img className='update-pen' onClick={clickUpdateUser} src='https://cdn.iconscout.com/icon/free/png-512/pencil-60-119100.png' />
                         }
@@ -31,6 +40,8 @@ const User = ({ user, updateUser, isUserPage, moveToTrash, deleteUser }) => {
                             <button className="delete-btn" onClick={moveToDeletedUsers}>X</button>}
                 </div>
 
+                {isShowTickets && <AllTicketsByUser tickets={user.tickets} userId={user.id}/>}
+                </>
             :
                 <div className="user-container">
                     <input className="update-input" value={inputValue} onChange={e => handlerChange(e)} />
