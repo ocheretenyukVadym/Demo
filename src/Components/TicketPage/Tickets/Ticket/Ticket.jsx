21a26a8ccc7028252afb45d87f8128347ed67cd9
Ocheretenyuk Vadym <72740686+ocheretenyukVadym@users.noreply.github.com>
2020-11-08 19:42:50 +0200

    Update Ticket.jsx

--- src/Components/TicketPage/Tickets/Ticket/Ticket.jsx
+++ src/Components/TicketPage/Tickets/Ticket/Ticket.jsx
@@ -39,21 +39,6 @@ const Ticket = ({ticket, updateTicket, isTicketPage, moveToTrash, deleteTicket, 
                             <button className="delete-btn" onClick={moveToRecycleBin}><img src="https://lh3.googleusercontent.com/proxy/OF10tCa-I2uSxpXBRaoL6-ByM0np0a3WtP0dvqujpqouhJCt3mkidH76eZ035yNPRAfSUgZcYmWApW0W7RIUMTwJ4wy3BzaJmr-095CHcHjrM1NPDnj3xZoyUSNQxRlyT9xf65RH"/></button> :
                             <button className="delete-btn" onClick={moveToDeletedTickets}><img src="https://lh3.googleusercontent.com/proxy/OF10tCa-I2uSxpXBRaoL6-ByM0np0a3WtP0dvqujpqouhJCt3mkidH76eZ035yNPRAfSUgZcYmWApW0W7RIUMTwJ4wy3BzaJmr-095CHcHjrM1NPDnj3xZoyUSNQxRlyT9xf65RH"/></button>}
                 </div>
-{/* 
-                <div>
-                    {isTicketPage ? "" :
-                        <div className="restore-button" onClick={restoreFromTrash}>restore</div>
-                    }
-
-                    <p>{ticket.title}</p>
-                    {isTicketPage &&
-                    <img className='update-pen' onClick={clickUpdateTicket}
-                         src='https://cdn.iconscout.com/icon/free/png-512/pencil-60-119100.png'/>
-                    }
-                    {isTicketPage ?
-                        <button className="delete-btn" onClick={moveToRecycleBin}>X</button> :
-                        <button className="delete-btn" onClick={moveToDeletedTickets}>X</button>}
-                </div> */}
                 </>
                 :
                 <div className="ticket-container">
