10bdc31ab28b3c7890ef5c296af2848fcd615b70
Martyn Kaminetskyi <Martyn.Kaminetskyi.kn.2017@lpnu.ua>
2020-11-08 19:37:53 +0200

    set ico +'commented code'

--- src/Components/TicketPage/Tickets/Ticket/Ticket.jsx
+++ src/Components/TicketPage/Tickets/Ticket/Ticket.jsx
@@ -28,6 +28,7 @@ const Ticket = ({ticket, updateTicket, isTicketPage, moveToTrash, deleteTicket, 
     return (
         <div>
             {!isViewChanges ?
+            <>
                 <div className="ticket-container">
                         <p>{ticket.title}</p>
                         <p>{converDate(date)}</p>
@@ -38,8 +39,8 @@ const Ticket = ({ticket, updateTicket, isTicketPage, moveToTrash, deleteTicket, 
                             <button className="delete-btn" onClick={moveToRecycleBin}><img src="https://lh3.googleusercontent.com/proxy/OF10tCa-I2uSxpXBRaoL6-ByM0np0a3WtP0dvqujpqouhJCt3mkidH76eZ035yNPRAfSUgZcYmWApW0W7RIUMTwJ4wy3BzaJmr-095CHcHjrM1NPDnj3xZoyUSNQxRlyT9xf65RH"/></button> :
                             <button className="delete-btn" onClick={moveToDeletedTickets}><img src="https://lh3.googleusercontent.com/proxy/OF10tCa-I2uSxpXBRaoL6-ByM0np0a3WtP0dvqujpqouhJCt3mkidH76eZ035yNPRAfSUgZcYmWApW0W7RIUMTwJ4wy3BzaJmr-095CHcHjrM1NPDnj3xZoyUSNQxRlyT9xf65RH"/></button>}
                 </div>
-
-
+{/* 
+                <div>
                     {isTicketPage ? "" :
                         <div className="restore-button" onClick={restoreFromTrash}>restore</div>
                     }
@@ -52,12 +53,15 @@ const Ticket = ({ticket, updateTicket, isTicketPage, moveToTrash, deleteTicket, 
                     {isTicketPage ?
                         <button className="delete-btn" onClick={moveToRecycleBin}>X</button> :
                         <button className="delete-btn" onClick={moveToDeletedTickets}>X</button>}
-                </div>
+                </div> */}
+                </>
                 :
                 <div className="ticket-container">
                     <input className="update-input" value={inputValue} onChange={e => handlerChange(e)}/>
                     <button className="save-btn" onClick={saveTicket}>save</button>
-                </div>}
+                </div>
+            }
+            
         </div>
     )
 }
