761546f5ec4e87a2922df5c7b508db405f7f834a
Martyn Kaminetskyi <Martyn.Kaminetskyi.kn.2017@lpnu.ua>
2020-11-09 16:45:42 +0200

    style changes in assign user element in ticket

--- src/Components/TicketPage/Tickets/Ticket/AssignToUserSelect.jsx
+++ src/Components/TicketPage/Tickets/Ticket/AssignToUserSelect.jsx
@@ -1,7 +1,6 @@
 import { inject, observer } from 'mobx-react';
 import React from 'react';
 import Select from 'react-select'
-
 const AssignToUserSelect = ({ store, ticketId }) => {
     const options = [];
 
@@ -17,11 +16,41 @@ const AssignToUserSelect = ({ store, ticketId }) => {
     })()
 
     return (
-        <div className="assign-user-container">
+        <div className="assign-user-container" style={{display: 'flex'}}>
             <Select isLoading={!store.isFetching} options={options} onChange={ user => 
-                    assignToUser(user.value)} className="assign-user"/>
+                    assignToUser(user.value)} styles={customStyles} />
         </div>
     )
 }
 
 export default inject("store")(observer(AssignToUserSelect))
+
+const customStyles = {
+    menu: (provided, state) => ({
+      ...provided,
+      borderBottom: '1px dotted pink',
+      color: state.selectProps.menuColor,
+      padding: 20,
+    }),
+
+    loadingIndicator: () => ({
+        display:'flex',
+        alignItems: 'center',
+        width: '50%',
+        height: '44%',
+        span: {
+            height: '50%',
+        }
+    }),
+  
+    control: (_, { selectProps: {  }}) => ({
+      display: 'flex',
+      border: '1px solid #dbdbdb',
+    }),
+  
+    singleValue: (provided, state) => {
+      const opacity = state.isDisabled ? 0.5 : 1;
+      const transition = 'opacity 300ms';
+      return { ...provided, opacity, transition };
+    }
+  }
